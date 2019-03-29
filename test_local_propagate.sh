0d9f5cb54a777fbea3544b4b945d61eef321ff4e
gayany <gayank@wso2.com>
2019-03-29 17:42:11 +0530

    [Phase 2] Exporting API Prefix environment variable

--- test_local_propagate.sh
+++ test_local_propagate.sh
@@ -10,6 +10,7 @@ export WSO2_APIM_ENV1_APIMGT_URL="https://172.17.0.1:9443"
 export WSO2_APIM_ENV1_GW_URL="https://172.17.0.1:8243"
 export WSO2_APIM_ENV1_APIMGT_USERNAME="admin"
 export WSO2_APIM_ENV1_APIMGT_PASSWD="admin"
+# WSO2_APIM_ENV1_ID should be lowercase
 export WSO2_APIM_ENV1_ID="dev1"
 export WSO2_APIM_ENV1_APIMGT_OWNER="admin"
 
@@ -17,8 +18,12 @@ export WSO2_APIM_ENV2_APIMGT_URL="https://172.17.0.1:9543"
 export WSO2_APIM_ENV2_GW_URL="https://172.17.0.1:8343"
 export WSO2_APIM_ENV2_APIMGT_USERNAME="admin"
 export WSO2_APIM_ENV2_APIMGT_PASSWD="admin"
-export WSO2_APIM_ENV2_ID="dev2"
+# WSO2_APIM_ENV2_ID should be lowercase
+export WSO2_APIM_ENV2_ID="dev11"
 export WSO2_APIM_ENV2_APIMGT_OWNER="admin"
+
+# WSO2_APIM_API_PREFIX should be lowercase
+export WSO2_APIM_API_PREFIX="cjams"
 
 export WSO2_APIM_VERIFY_SSL="False"
 
